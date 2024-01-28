7640fb98dc685a04cdec2747cd628ca409d45a52
Kopynus <102612927+MaksShahov@users.noreply.github.com>
2024-01-28 17:18:09 +0200

    7 day

---
+++ GGJ 2024/Assets/Scripts/ControlsVanish.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ControlsVanish : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    private void Start()
+    {
+        Invoke(nameof(VanishControls), 7f);
+    }
+
+    private void VanishControls()
+    {
+        text.DOFade(0, 1);
+    }
+}
